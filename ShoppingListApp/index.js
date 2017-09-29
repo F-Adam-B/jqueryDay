391709916370cea4777534923f7d702cb5ab7c8a
Adam Bradbury <f.adambrad@gmail.com>
2017-09-29 14:20:25 -0600

    shoppinglistReplit

--- ShoppingListApp/index.js
+++ ShoppingListApp/index.js
@@ -1,12 +1,23 @@
 // create function when adding an item
-
+// put in seperate function
     $('button').click(function() {
         // call form element and apply submit method
        $('form').submit(function(event) {
-        const newItem = $('.shopping-list').prepend('<li> does this work');
+        const newItem = $('.shopping-list').prepend(`<li>
+            <span class="shopping-item">${newItem}</span>
+            <div class="shopping-item-controls">
+              <button class="shopping-item-toggle">
+                <span class="button-label">check</span>
+              </button>
+              <button class="shopping-item-delete">
+                <span class="button-label">delete</span>
+              </button>
+            </div>
+          </li>`
+        });
         event.preventDefault(); 
         // newItem.addClass('shopping-item ');   
-       });     
+        
     });
 // delete shopping item after clicking delete
     $('button.shopping-item-delete').click(function() {
