7c8a0b2c8fe4e0a9aa6fb1764f455ba584fd4f5b
Adam Bradbury <f.adambrad@gmail.com>
2017-09-28 21:05:43 -0600

    shopping list last

--- ShoppingListApp/index.js
+++ ShoppingListApp/index.js
@@ -1,18 +1,23 @@
 // create function when adding an item
 
-    $('button').click(function() {
+    $('form:third-child').closest('ul').click(function() {
         // call form element and apply submit method
        $('form').submit(function(event) {
         const newItem = $('.shopping-list').prepend('<li> does this work');
-         newItem.addClass('shopping-item ');
-
-          
-           event.preventDefault();
-    });
-
-    $('.shopping-item-delete').click, (function() {
+        event.preventDefault(); 
+        newItem.addClass('shopping-item ');    
+       });     
+    
+// delete shopping item after clicking delete
+    $('button.shopping-item-delete').click(function() {
             $(this).remove();
     })
+
+// strikethrough list item once check button clicked
+
+    $('shopping-item-toggle').click(function() {
+        $('shopping-item').addClass('.shopping-item__checked');
+    
 });
 
 
